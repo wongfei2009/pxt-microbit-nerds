3e65d733c4c717213c0904a5ff87c09a668f7ce5
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-07-01 21:12:52 +0900

    更新 main.blocks, nerds.ts

--- nerds.ts
+++ nerds.ts
@@ -13,49 +13,9 @@ enum ButtonAction {
 //% block="Nerds"
 //% block.loc.ja="おたく" 
 //% weight=10
-//% color=#4682b4
+//% color=#696969
 //% icon="\uf164"
 namespace nerds {
-
-
-
-
-
-
-    // ----- Event
-
-    //% block="on custom event from $id with value$val"
-    //% block.loc.ja="カスタム・イベントが届いたとき 発生源$id 値$val"
-    //% weight=200
-    //% subcategory="event"
-    export function customEventHandler(id: number, val: number, handler: () => void) {
-        control.onEvent(id, val, handler)
-    }
-
-    //% block="raise custom event from source$id with value$val"
-    //% block.loc.ja="カスタム・イベントを発生させる 発生源$id 値$val"
-    //% weight=100
-    //% subcategory="event"
-    export function customEventFire(id: number, val: number) {
-        control.raiseEvent(id, val)
-    }
-
-    //% block="event timestamp(uSec)"
-    //% block.loc.ja="イベントのタイムスタンプ（μ秒）"
-    //% weight=50
-    //% subcategory="event"
-    export function customEventTimestamp() {
-        return control.eventTimestamp()
-    }
-
-    //% block="event value"
-    //% block.loc.ja="イベントの値"
-    //% weight=40
-    //% subcategory="event"
-    export function customEventValue() {
-        return control.eventValue()
-    }
-
 
 
 
@@ -69,6 +29,46 @@ namespace nerds {
     export function onButton(button: Button, action: ButtonAction, handler: () => void) {
         control.onEvent(button, action, handler)
     }
+
+
+
+
+
+
+    // ----- Event
+
+    //% block="on event from $id with value$val"
+    //% block.loc.ja="イベントが届いたとき 発生源$id 値$val"
+    //% weight=200
+    //% subcategory="event"
+    export function eventHandler(id: number, val: number, handler: () => void) {
+        control.onEvent(id, val, handler)
+    }
+
+    //% block="raise event from source$id with value$val"
+    //% block.loc.ja="イベントを発生させる 発生源$id 値$val"
+    //% weight=100
+    //% subcategory="event"
+    export function eventFire(id: number, val: number) {
+        control.raiseEvent(id, val)
+    }
+
+    //% block="event timestamp(uSec)"
+    //% block.loc.ja="イベントのタイムスタンプ（μ秒）"
+    //% weight=50
+    //% subcategory="event"
+    export function eventTimestamp() {
+        return control.eventTimestamp()
+    }
+
+    //% block="event value"
+    //% block.loc.ja="イベントの値"
+    //% weight=40
+    //% subcategory="event"
+    export function eventValue() {
+        return control.eventValue()
+    }
+
 
 
 
