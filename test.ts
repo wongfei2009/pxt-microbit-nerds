2867368ea0b320e3dfb9515b1584e379df7ac100
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-09-29 02:36:07 +0900

    更新 pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 // テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
 input.onButtonPressed(Button.A, function () {
-    nerds.playCustomMelody(nerds.stringToMelody("d5:2,d5:2,r:2,d5:4,c5:2,a4:2,a4:2,,c5:2,c5:2,r:2,c5:4,a4:2,g4:2,f4:2"), MelodyOptions.Once)
+    nerds.playCustomMelody(nerds.stringToMelody("d5:2,d5:2,r:2,d5:4,c5:2,a4:2,a4:2,,c5:2,c5:2,r:2,c5:4,a4:2,g4:2,f4:2,g4:2,g4:2,g4:2,g4:2,g4:2,e4:2,c4:2,d4:2"), MelodyOptions.Once)
 })
