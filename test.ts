e3668bd6de5b48885f681113c0f1a7ea70c48701
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-09-29 19:05:55 +0900

    更新 main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -2,7 +2,18 @@
 input.onButtonPressed(Button.A, function () {
     nerds.playCustomMelody(haruichiban, MelodyOptions.Once)
 })
+input.onButtonPressed(Button.B, function () {
+    nerds.playCustomMelody(kirakiraboshi, MelodyOptions.Once)
+})
 let haruichiban: string[] = []
 haruichiban = nerds.stringToMelody(
     "d5:2,d5:2,r:2,d5:4,c5:2,a4:2,a4:2,,c5:2,c5:2,r:2,c5:4,a4:2,g4:2,f4:2," + 
     "g4:1,r:1,g4:1,r:1,g4:1,r:1,g4:1,r:1,g4:1,r:1,e4:2,c4:2,d4:2")
+let kirakiraboshi: string[] = []
+kirakiraboshi = nerds.stringToMelody(
+    "g4:4,g4:4,d5:4,d5:4,e5:4,e5:4,d5:4,r:4," + 
+    "c5:4,c5:4,b4:4,b4:4,a4:4,a4:4,g4:4,r:4," +
+    "d5:4,d5:4,c5:4,c5:4,b4:4,b4:4,a4:4,r:4," + 
+    "d5:4,d5:4,c5:4,c5:4,b4:4,b4:4,a4:4,r:4," +
+    "g4:4,g4:4,d5:4,d5:4,e5:4,e5:4,d5:4,r:4," +
+    "c5:4,c5:4,b4:4,b4:4,a4:4,a4:4,g4:4,r:4")
