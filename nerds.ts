f02b9e29307b0156ef25ff1b9edd599cdd00d48a
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-10-01 23:25:09 +0900

    更新 pxt.json, main.blocks, nerds.ts

--- nerds.ts
+++ nerds.ts
@@ -1,4 +1,119 @@
 
+enum JNote {
+    //% block="C4"
+    //% block.loc.ja="ド"
+    c4,
+    //% block="C#4"
+    //% block.loc.ja="ド#"
+    cs4,
+    //% block="D4"
+    //% block.loc.ja="レ"
+    d4,
+    //% block="D#4"
+    //% block.loc.ja="レ#"
+    ds4,
+    //% block="E4"
+    //% block.loc.ja="ミ"
+    e4,
+    //% block="F4"
+    //% block.loc.ja="ファ"
+    f4,
+    //% block="F#4"
+    //% block.loc.ja="ファ#"
+    fs4,
+    //% block="G4"
+    //% block.loc.ja="ソ"
+    g4,
+    //% block="G#4"
+    //% block.loc.ja="ソ#"
+    gs4,
+    //% block="A4"
+    //% block.loc.ja="ラ"
+    a4,
+    //% block="A#4"
+    //% block.loc.ja="ラ#"
+    as4,
+    //% block="B4"
+    //% block.loc.ja="シ"
+    b4,
+
+    //% block="C3"
+    //% block.loc.ja="ド3"
+    c3,
+    //% block="C#3"
+    //% block.loc.ja="ド#3"
+    cs3,
+    //% block="D3"
+    //% block.loc.ja="レ3"
+    d3,
+    //% block="D#3"
+    //% block.loc.ja="レ#3"
+    ds3,
+    //% block="E3"
+    //% block.loc.ja="ミ3"
+    e3,
+    //% block="F3"
+    //% block.loc.ja="ファ3"
+    f3,
+    //% block="F#3"
+    //% block.loc.ja="ファ#3"
+    fs3,
+    //% block="G3"
+    //% block.loc.ja="ソ3"
+    g3,
+    //% block="G#3"
+    //% block.loc.ja="ソ#3"
+    gs3,
+    //% block="A3"
+    //% block.loc.ja="ラ3"
+    a3,
+    //% block="A#3"
+    //% block.loc.ja="ラ#3"
+    as3,
+    //% block="B3"
+    //% block.loc.ja="シ3"
+    b3,
+
+    //% block="C5"
+    //% block.loc.ja="ド5"
+    c5,
+    //% block="C#5"
+    //% block.loc.ja="ド#5"
+    cs5,
+    //% block="D5"
+    //% block.loc.ja="レ5"
+    d5,
+    //% block="D#5"
+    //% block.loc.ja="レ#5"
+    ds5,
+    //% block="E5"
+    //% block.loc.ja="ミ5"
+    e5,
+    //% block="F5"
+    //% block.loc.ja="ファ5"
+    f5,
+    //% block="F#5"
+    //% block.loc.ja="ファ#5"
+    fs5,
+    //% block="G5"
+    //% block.loc.ja="ソ5"
+    g5,
+    //% block="G#5"
+    //% block.loc.ja="ソ#5"
+    gs5,
+    //% block="A5"
+    //% block.loc.ja="ラ5"
+    a5,
+    //% block="A#5"
+    //% block.loc.ja="ラ#5"
+    as5,
+    //% block="B5"
+    //% block.loc.ja="シ5"
+    b5,
+
+}
+
+
 //% block="Nerds"
 //% block.loc.ja="おたく"
 //% weight=10
@@ -201,5 +316,15 @@ namespace nerds {
         return melody
     }
 
-
+    //% block="%jnote"
+    //% block.loc.ja="%jnote"
+    //% weight=220
+    //% subcategory="melody"
+    //% group=""
+    //% jnote.fieldEditor="gridpicker"
+    //% jnote.fieldOptions.width=220
+    //% jnote.fieldOptions.columns=12
+    export function getMelodyString(jnote: JNote) {
+        return "s"
+    }
 }
