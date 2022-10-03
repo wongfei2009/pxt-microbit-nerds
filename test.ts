0a0f220c1cdbb4bb5e6d730e374719ddc59074d5
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-10-03 20:02:21 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@
 input.onButtonPressed(Button.A, function () {
     // let tempo = music.tempo()
     music.setTempo(240)
-    nerds.playNoteArray(haruichiban, MelodyOptions.Once)
+    nerds.playNoteArray(haruichiban2, MelodyOptions.Once)
     // music.setTempo(tempo)
 })
 input.onButtonPressed(Button.B, function () {
@@ -14,12 +14,40 @@ input.onButtonPressed(Button.AB, function () {
     nerds.playNoteArray(kirakiraboshi2, MelodyOptions.Once)
 })
 let haruichiban: string[] = []
-// haruichiban = nerds.stringToNoteArray(
-//     "d5:2,d5:2,r:2,d5:4,c5:2,a4:2,a4:2,c5:2,c5:2,r:2,c5:4,a4:2,g4:2,f4:2," +
-//     "g4:1,r:1,g4:1,r:1,g4:1,r:1,g4:1,r:1,g4:1,r:1,e4:2,c4:2,d4:2")
 haruichiban = nerds.stringToNoteArray(
+    "d5:2,d5:4,d5:4,c5:2,a4:2,a4:2,c5:2,c5:4,c5:4,a4:2,g4:2,f4:2," +
+    "g4:2,g4:2,g4:2,g4:2,g4:2,e4:2,c4:2,d4:10,r:2," +
+    "d5:2,d5:4,d5:4,c5:2,a4:2,a4:2,c5:2,c5:4,c5:4,a4:2,g4:2,f4:2," +
+    "g4:2,g4:8,g4:2,e4:2,c4:2,d4:10,r:2," +
+    "a4:4,c5:2,c5:2,d5:8,c5:2,a4:2,a4:2,c5:2,a4:2,r:4," +
+    "a4:2,r:2,a4:2,g4:2,g4:2,r:1,g4:2,e4:2,e4:2,e4:2,c4:2,d4:2,r:2," +
+    "g4:4,g4:2,g4:2,f4:2,g4:2,a4:2,c5:2,r:2,a4:2,r:4," +
+    "d5:4,d5:2,d5:2,d5:2,d5:2,a4:2,a4:2,c5:2,c5:4,r:2," +
+    "g4:2,g4:2,g4:2,g4:2,f4:2,g:4.r:1,a4:2,c5:2,r:2,a4:2,r:4," +
+    "a4:2,a4:2,a4:2,a4:2,c5:2,g4:2,r:2,a4:4,r:4," +
+    "a4:4,c5:2,c5:2,d5:8,c5:2,a4:2,a4:2,c5:2,a4:2,r:4," +
+    "a4:2,r:2,a4:2,g4:2,g4:2,g4:2,g4:2,e4:2,c4:2,r:2,d4:2,r:4," +
+    "a4:4,c5:2,c5:2,d5:8,c5:2,a4:2,a4:2,c5:2,a4:2,r:4," +
+    "a4:2,r:2,a4:2,g4:2,g4:2,g4:2,g4:2,e4:2,c4:2,r:2,d4:2,r:2," +
+    "")
+let haruichiban2: string[] = []
+haruichiban2 = nerds.stringToNoteArray(
     "d5:2,r:1,d5:4,r:1,d5:4,r:1,c5:2,r:1,a4:2,r:1,a4:2,r:1,c5:2,r:1,c5:4,r:1,c5:4,r:1,a4:2,r:1,g4:2,r:1,f4:2,r:1," +
-    "g4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,e4:2,r:1,c4:2,r:1,d4:2")
+    "g4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,e4:2,r:1,c4:2,r:1,d4:10,r:1,r:2,r:1," +
+    "d5:2,r:1,d5:4,r:1,d5:4,r:1,c5:2,r:1,a4:2,r:1,a4:2,r:1,c5:2,r:1,c5:4,r:1,c5:4,r:1,a4:2,r:1,g4:2,r:1,f4:2,r:1," +
+    "g4:2,r:1,g4:8,r:1,g4:2,r:1,e4:2,r:1,c4:2,r:1,d4:10,r:1,r:2,r:1," +
+    "a4:4,r:1,c5:2,r:1,c5:2,r:1,d5:8,r:1,c5:2,r:1,a4:2,r:1,a4:2,r:1,c5:2,r:1,a4:2,r:1,r:4,r:1," +
+    "a4:2,r:1,r:2,r:1,a4:2,r:1,g4:2,r:1,g4:2,r:1,r:1,r:1,g4:2,r:1,e4:2,r:1,e4:2,r:1,e4:2,r:1,c4:2,r:1,d4:2,r:1,r:2,r:1," +
+    "g4:4,r:1,g4:2,r:1,g4:2,r:1,f4:2,r:1,g4:2,r:1,a4:2,r:1,c5:2,r:1,r:2,r:1,a4:2,r:1,r:4,r:1," +
+    "d5:4,r:1,d5:2,r:1,d5:2,r:1,d5:2,r:1,d5:2,r:1,a4:2,r:1,a4:2,r:1,c5:2,r:1,c5:4,r:1,r:2,r:1," +
+    "g4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,f4:2,r:1,g:4.r:1,r:1,a4:2,r:1,c5:2,r:1,r:2,r:1,a4:2,r:1,r:4,r:1," +
+    "a4:2,r:1,a4:2,r:1,a4:2,r:1,a4:2,r:1,c5:2,r:1,g4:2,r:1,r:2,r:1,a4:4,r:1,r:4,r:1," +
+    "a4:4,r:1,c5:2,r:1,c5:2,r:1,d5:8,r:1,c5:2,r:1,a4:2,r:1,a4:2,r:1,c5:2,r:1,a4:2,r:1,r:4,r:1," +
+    "a4:2,r:1,r:2,r:1,a4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,e4:2,r:1,c4:2,r:1,r:2,r:1,d4:2,r:1,r:4,r:1," +
+    "a4:4,r:1,c5:2,r:1,c5:2,r:1,d5:8,r:1,c5:2,r:1,a4:2,r:1,a4:2,r:1,c5:2,r:1,a4:2,r:1,r:4,r:1," +
+    "a4:2,r:1,r:2,r:1,a4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,g4:2,r:1,e4:2,r:1,c4:2,r:1,r:2,r:1,d4:2,r:1,r:2,r:1," +
+    "")
+
 let kirakiraboshi: string[] = []
 kirakiraboshi = nerds.stringToNoteArray(
     "c4:4,r:1,c4:4,r:1,g4:4,r:1,g4:4,r:1,a4:4,r:1,a4:4,r:1,g4:4,r:1,r:4,r:1," +
